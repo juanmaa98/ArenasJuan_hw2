94a40ef10fd7f6bc723534e45ce0fada8ed7fa06
Juan <jm.arenas10@uniandes.edu.co>
2019-04-19 19:52:22 -0500

    Codigo con la verificacion de la transformada con el paquete de numpy

--- Fourier.py
+++ Fourier.py
@@ -31,3 +31,53 @@ plt.tight_layout()
 
 
 plt.savefig("ondas.pdf")
+
+#Definicion propia de la transformada de fourier
+def TFourier(ft,t):
+	N=len(ft)
+	dt=t[10]-t[9]
+	F=np.zeros((N),complex)
+	freq=np.zeros((N),float)
+	for n in range(0,N):
+		for k in range(0,N):
+			F[n]+=ft[k]*np.exp(-1j*2*np.pi*k*n/N)
+		freq[n]=n/(k*dt)
+	F2=F[0:int(N/2)]*2/np.mean(F)
+	freq2=freq[0:int(N/2)]
+
+	return F2,freq2
+
+F1,freq1=TFourier(ft1,t1)
+F2,freq2=TFourier(ft2,t2)
+
+#Se grafican ambas transformadas de fourier
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(freq2,F2)
+plt.title("Senal")
+plt.xlabel("frecuencia")
+plt.ylabel("F")
+
+plt.subplot(2,1,2)
+plt.plot(freq1,F1,'orange')
+plt.title("Suma de las ondas")
+plt.xlabel("frecuencia")
+plt.ylabel("F")
+
+plt.tight_layout()
+
+fourier = np.fft.fft(ft2)
+freq=np.fft.fftfreq(len(ft2),d=t2[10]-t2[9])
+
+plt.figure()
+plt.subplot(211)
+plt.plot(freq[0:int(len(t2)/2-1)],fourier[0:int(len(t2)/2-1)],"-",label="fftfreq")
+plt.subplot(212)
+plt.plot(freq1,F1,'orange')
+plt.title("Suma de las ondas")
+plt.xlabel("frecuencia")
+plt.ylabel("F")
+
+plt.show()
+
+
