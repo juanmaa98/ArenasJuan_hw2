1a3d96f1c0bc829d11b407030d68a81fa0fba61b
Juan <jm.arenas10@uniandes.edu.co>
2019-05-09 20:00:55 -0500

    Make con el primer y segundo punto de la tarea

--- hmw2.mk
+++ hmw2.mk
@@ -1,3 +1,8 @@
+Resultados_hw2.pdf: EspecTemblor.pdf TransformadaTemplor.pdf temblor.pdf EspectroSignal.pdf EspectroSum.pdf transformadasOndas.pdf ondas.pdf uvsw.pdf umax.dat
+	pdflatex Resultados_hw2.tex
+EspecTemblor.pdf TransformadaTemplor.pdf temblor.pdf EspectroSignal.pdf EspectroSum.pdf transformadasOndas.pdf ondas.pdf: Fourier.py temblor.txt signal.dat signalSuma.dat
+	python3 Fourier.py
+
 uvsw.pdf : Plots_hw2.py umax.dat
 	python3 Plots_hw2.py
 
