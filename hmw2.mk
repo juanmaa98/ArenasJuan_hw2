eb877d570247449cd9838e6468875d2ba825a244
Juan <jm.arenas10@uniandes.edu.co>
2019-05-09 20:19:39 -0500

    Make con todos los archivos necesarios

--- hmw2.mk
+++ hmw2.mk
@@ -1,6 +1,18 @@
-Resultados_hw2.pdf: EspecTemblor.pdf TransformadaTemplor.pdf temblor.pdf EspectroSignal.pdf EspectroSum.pdf transformadasOndas.pdf ondas.pdf uvsw.pdf umax.dat
+Resultados_hw2.pdf: EspecTemblor.pdf TransformadaTemblor.pdf temblor.pdf EspectroSignal.pdf EspectroSum.pdf transformadasOndas.pdf ondas.pdf uvsw.pdf Resultados_hw2.tex
 	pdflatex Resultados_hw2.tex
-EspecTemblor.pdf TransformadaTemplor.pdf temblor.pdf EspectroSignal.pdf EspectroSum.pdf transformadasOndas.pdf ondas.pdf: Fourier.py temblor.txt signal.dat signalSuma.dat
+EspecTemblor.pdf : Fourier.py temblor.txt signal.dat signalSuma.dat
+	python3 Fourier.py
+TransformadaTemblor.pdf : Fourier.py temblor.txt signal.dat signalSuma.dat
+	python3 Fourier.py
+temblor.pdf : Fourier.py temblor.txt signal.dat signalSuma.dat
+	python3 Fourier.py
+EspectroSignal.pdf : Fourier.py temblor.txt signal.dat signalSuma.dat
+	python3 Fourier.py
+EspectroSum.pdf : Fourier.py temblor.txt signal.dat signalSuma.dat
+	python3 Fourier.py
+transformadasOndas.pdf : Fourier.py temblor.txt signal.dat signalSuma.dat
+	python3 Fourier.py
+ondas.pdf: Fourier.py temblor.txt signal.dat signalSuma.dat
 	python3 Fourier.py
 
 uvsw.pdf : Plots_hw2.py umax.dat
